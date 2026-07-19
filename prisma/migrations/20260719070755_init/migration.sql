08713e88336f0d3f5d871b90f7efd8a6f6fd71d8
Niket Sarkar <niketsarkar@Nikets-MacBook-Air.local>
2026-07-20 00:00:24 +0530

    feat: backend prototype APIs and Clerk auth proxy setup

---
+++ prisma/migrations/20260719070755_init/migration.sql
@@ -0,0 +1,33 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "clerkId" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "firstName" TEXT,
+    "lastName" TEXT,
+    "imageUrl" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Video" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "publicId" TEXT NOT NULL,
+    "originalSize" TEXT NOT NULL,
+    "compressedSize" TEXT NOT NULL,
+    "duration" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Video_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_clerkId_key" ON "User"("clerkId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
---
+++ proxy.ts
@@ -0,0 +1,42 @@
+import { clerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export default clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth();
+  const { pathname } = req.nextUrl;
+
+  // Public pages
+  const isPublicRoute =
+    pathname === "/" ||
+    pathname.startsWith("/sign-in") ||
+    pathname.startsWith("/sign-up");
+
+  // Public APIs
+  const isPublicApiRoute = pathname.startsWith("/api/videos");
+
+  // Redirect authenticated users away from auth pages
+  if (
+    userId &&
+    (pathname.startsWith("/sign-in") || pathname.startsWith("/sign-up"))
+  ) {
+    return NextResponse.redirect(new URL("/home", req.url));
+  }
+
+  // Protect all non-public pages and APIs
+  if (!userId && !isPublicRoute && !isPublicApiRoute) {
+    await auth.protect();
+  }
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and static files
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+
+    // Always run for Clerk frontend API routes
+    "/__clerk/(.*)",
+  ],
+};
